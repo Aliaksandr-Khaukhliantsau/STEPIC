7e6bcb6a7125e500b9156e0016ee870f0685fb72
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-10-19 16:41:29 +0300

    durak solution

--- Durak.py
+++ Durak.py
@@ -1,6 +1,4 @@
-def func(card_1, card_2):
-    print(card_1, card_2)
-    #    return
+def result(card_1, card_2):
     if card_1[1] == card_2[1]:
         if int(card_1[0]) > int(card_2[0]):
             return 'First'
@@ -8,25 +6,48 @@ def func(card_1, card_2):
             return 'Second'
         else:
             return 'Error'
-
-
-def durak_card_1(val, card_1):
-    if val == '1':
-        card_1.append('10')
     else:
-        card_1.append(val)
-    return card_1
+        if card_1[1] == trump:
+            return 'First'
+        elif card_2[1] == trump:
+            return 'Second'
+        else:
+            return 'Error'
 
 
 def durak_card_2(val, card_2):
     if val == '1':
         card_2.append('10')
+    elif val == 'J':
+        card_2.append('11')
+    elif val == 'Q':
+        card_2.append('12')
+    elif val == 'K':
+        card_2.append('13')
+    elif val == 'A':
+        card_2.append('14')
     else:
         card_2.append(val)
     return card_2
 
 
-def durak(suits, values, cards, trump):
+def durak_card_1(val, card_1):
+    if val == '1':
+        card_1.append('10')
+    elif val == 'J':
+        card_1.append('11')
+    elif val == 'Q':
+        card_1.append('12')
+    elif val == 'K':
+        card_1.append('13')
+    elif val == 'A':
+        card_1.append('14')
+    else:
+        card_1.append(val)
+    return card_1
+
+
+def durak():
     card_1 = []
     card_2 = []
     for val in values:
@@ -41,14 +62,11 @@ def durak(suits, values, cards, trump):
         if suit == cards[1][-1]:
             card_2.append(suit)
 
-    func(card_1, card_2)
-
-
-#    return card_1, card_2
+    return result(card_1, card_2)
 
 
 suits = ['C', 'D', 'H', 'S']
 values = ['6', '7', '8', '9', '1', 'J', 'Q', 'K', 'A']
 cards = input().split()
 trump = input()
-print(durak(suits, values, cards, trump))
+print(durak())
