b336229d1dd925cafe92d01fe61a856fd0da7426
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-10-21 16:36:36 +0300

    for i in number_of_lines: error

--- minesweeper_field.py
+++ minesweeper_field.py
@@ -6,11 +6,11 @@ if len(net) != 2:
 if net[0].isdigit() is False or net[1].isdigit() is False:
     print('нужно вводить только числа')
 
-print(net)
+# print(net)
 
 n, m = int(net[0]), int(net[1])
 
-print(n, m)
+# print(n, m)
 
 if n < 1 or n > 100 or m < 1 or m > 100:
     print('числа n и m должны быть не меньше 1 и не больше 100')
@@ -24,8 +24,61 @@ for _ in range(m):
     if len(a) != n:
         print(f'кол-во вводимых элементов должно быть равно {n}')
         break
-    number_of_lines += tuple(a)
+    number_of_lines += (a,)
 
 # number_of_cells = n * m
 
-print(number_of_lines)
+# print(number_of_lines)
+# print(number_of_lines[-1])
+# print(number_of_lines[-1][-1])
+
+for i in number_of_lines:
+    # ??????????????????????????????
+    print(i)
+    for z in range(len(i)):
+        print(z)
+        print(number_of_lines[i][z])
+        if z == 0:
+            # print('ноль')
+            if i[z] == '.':
+                x = 0
+                # print('.')
+                if number_of_lines[i + 1][z] == '*':
+                    x += 1
+                if number_of_lines[i + 1][z + 1] == '*':
+                    x += 1
+                print(x)
+
+        elif z == len(i) - 1:
+            # print('минус 1')
+            if i[z] == '.':
+                x = 0
+                # print('.')
+                if number_of_lines[i + 1][z] == '*':
+                    x += 1
+                if number_of_lines[i + 1][z - 1] == '*':
+                    x += 1
+                print(x)
+
+        else:
+            if i[z] == '.':
+                x = 0
+                # print('другое')
+                if number_of_lines[i + 1][z] == '*':
+                    x += 1
+                    # print('.')
+                if number_of_lines[i + 1][z - 1] == 'x':
+                    x += 1
+                if number_of_lines[i + 1][z + 1] == 'x':
+                    x += 1
+                    print(x)
+
+
+
+
+
+
+
+
+if number_of_lines[-1]:
+    pass
