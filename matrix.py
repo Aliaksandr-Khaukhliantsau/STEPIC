e0bc2a4e23ecbbdfcb71859d10cd72165293fc2f
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-10-23 13:33:36 +0300

    check

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def func_5(result_tuple):
+def result_output(result_tuple):
     result = ''
     for y in range(len(result_tuple)):
         if y == 0:
@@ -11,7 +11,7 @@ def func_5(result_tuple):
     print(result)
 
 
-def func_4(i, matrix):
+def filling_a_result_tuple(i, matrix):
     for k in matrix:
         for z in k:
             if i == z:
@@ -22,15 +22,15 @@ def func_4(i, matrix):
     sys.exit()
 
 
-def func_3(matrix):
+def creating_a_result_tuple(matrix, word_input):
     result_tuple = ()
     for i in word_input:
-        result_tuple += (func_4(i, matrix),)
+        result_tuple += (filling_a_result_tuple(i, matrix),)
     print(result_tuple)
-    func_5(result_tuple)
+    result_output(result_tuple)
 
 
-def func_2(sqrt):
+def matrix_creation(word_input, string_input, sqrt):
     matrix = []
     a = 0
     b = sqrt
@@ -38,155 +38,80 @@ def func_2(sqrt):
         matrix.append(string_input[a:a + sqrt])
         a += sqrt
     print(matrix)
-    func_3(matrix)
+    creating_a_result_tuple(matrix, word_input)
 
 
-def func_1():
+def square_root_extraction(word_input, string_input):
     sqrt = len(string_input) ** .5
     print(sqrt)
     if sqrt.is_integer():
         sqrt = int(sqrt)
-        func_2(sqrt)
+        matrix_creation(word_input, string_input, sqrt)
     else:
         print('Строка должна описывать квадратную матрицу')
         return
 
 
+def word_validation(word_input, string_input):
+    if len(word_input) == 0:
+        print('Вы не ввели слово')
+        return
+    if word_input.isalpha():
+        print('word is alpha')
+        square_root_extraction(word_input, string_input)
+    else:
+        print('Слово должно содержать только символы алфавита')
+        return
 
 
+def word_input_function(string_input):
+    print('Введите слово: ')
+    word_input = input()
+    word_validation(word_input, string_input)
 
-def line_input_function():
-    print('Введите строку: ')
-    string_input = input()
-    # checking_a_string_for_the_alphabet()
+
+def string_validation(string_input):
     if len(string_input) < 4:
         print('Минимальное количество символов крадратной матрицы должно быть 2 * 2')
         return
     if string_input.isalpha():
         print('string is alpha')
-        print('Введите слово: ')
-        word_input = input()
-        if len(word_input) == 0:
-            print('Вы не ввели слово')
-            return
-        if word_input.isalpha():
-            print('word is alpha')
-            func_1()
-        else:
-            print('Слово должно содержать только символы алфавита')
-            return
+        word_input_function(string_input)
     else:
         print('Строка должна содержать только символы алфавита')
         return
 
 
-# string_input = input()
-# word_input = input()
+def line_input_function():
+    print('Введите строку: ')
+    string_input = input()
+    string_validation(string_input)
 
-# string_input = 'OMNZ'
-# string_input = 'CVBSYPLME'
-# string_input = 'QLGNAEKIRLRNGEAE'
-# string_input = 'WKDODJHFGHGKDDKJXOQTDNTES'
-# string_input = 'LPOIUHBVFGGFGGBVVCCCFFDWQAZMJDTUIRSD'
-# string_input = 'LPOIUHBVFFGGFGGBVVCCCFFDWQAZMJDTUIRSD'
-# string_input = 'EPIBLKHHGDLWHLKWLRDWJDMNBMNBVTHQYUIWOSYONDQKXZPMA'
-# string_input = 'LEHWLKHCNQIUUJNBZLALSKKJQKJQQQSBCNCGNBMBBZXBJHKJHMNBVQWJDYTAKOPR'
-# string_input = 'QLGCAEKIRLRUGEA'
-# string_input = 'QLGNAEK1ILRNGEAE'
-# string_input = 'eQGNAEKrILRNGEAE'
-# string_input = 'QLGNAEKI{LRNGEA'
-# string_input = 'LG.NAEKILLRNGEA'
-# string_input = 'LGNAEKIL LRNGEA'
-# string_input = ''
-# string_input = ''
-# string_input = '      '
-# string_input = 'W'
-# string_input = 'WE'
-# string_input = 'WEF'
-
-# word_input = 'MZ'
-# word_input = 'OMNZ'
-# word_input = 'OMNZM'
-# word_input = 'CSEV'
-# word_input = 'KING'
-# word_input = 'NOTES'
-# word_input = 'REGKQ'
-# word_input = ''
-# word_input = ' '
-# word_input = 'CQDJUOPSZXBOPD'
-# word_input = 'QUICK'
-# word_input = 'QUIC1'
-# word_input = 'KING'
-# word_input = 'KING'
-
-
-# print(string_input)
-# print(len(string_input))
-# print(word_input)
 
 line_input_function()
-# func_1()
-# func_2()
 
-
-# if len(string_input) < 4:
-#     print('Строка должна описывать квадратную матрицу с минимальным количеством символов 2 * 2')
-#     if string_input.isalpha():
-#         print('string is alpha')
-#         # word_input = input()
-#         if len(word_input) == 0:
-#             print('Вы не ввели слово')
-#         if word_input.isalpha():
-#             print('word is alpha')
-#         else:
-#             print('word isn\'t alpha')
-#             # return
-#     else:
-#         print('string is\'t alpha')
-#         # return
-
-
-# sqrt = len(string_input) ** .5
-# print(sqrt)
-
-# if sqrt.is_integer():
-#     sqrt = int(sqrt)
-
-# matrix = []
-#
-# a = 0
-# b = sqrt
-# for _ in range(sqrt):
-#     matrix.append(string_input[a:a + sqrt])
-#     a += sqrt
-#
-# print(matrix)
-
-
-# def func_4(i):
-#     for k in matrix:
-#         for z in k:
-#             if i == z:
-#                 w = matrix.index(k)
-#                 q = k.index(i)
-#                 return w, q
-#
-#     print('Такой буквы нет в матрице')
-#     return
-
-
-# result_tuple = ()
-# for i in word_input:
-#     result_tuple += (func_4(i),)
-#
-# print(result_tuple)
-
-
-# result = ''
-# for i in range(len(result_tuple)):
-#     if i == 0:
-#         result += f'[{result_tuple[i][0]},{result_tuple[i][1]}]'
-#     else:
-#         result += f' -> [{result_tuple[i][0]},{result_tuple[i][1]}]'
-#
-# print(result)
+# 'OMNZ' 'MZ' [0,1] -> [1,1]
+# string_input = 'CVBSYPLME'
+# 'QLGNAEKIRLRNGEAE' 'KING' [1,2] -> [1,3] -> [0,3] -> [0,2]
+# 'WKDODJHFGHGKMDKJXOQTDNTES' 'OMNO' [0,3] -> [2,2] -> [4,1] -> [0,3]
+# 'LPOIUHBVFGGFGGBVVCCCFFDWQAZMJDTUIRSD' 'OMWZM' [0,2] -> [4,3] -> [3,5] -> [4,2] -> [4,3]
+# 'EPIBLKHHGDLWHLKWLRDWJDMNBMNBVTHQYUIWOSYONDQKXZPMA' 'NOTES' [3,2] -> [5,1] -> [4,1] -> [0,0] -> [5,2]
+# 'LPOIUHBVFGGFGGBVVCCCFFDWQAZMJDTUIRSD' 'OMwZM' Буквы 'w' нет в матрице
+# 'LEHWLKHCNQIUUJNBZLALSKKJQKJQQQSBCNCGNBMBBZXBJHKJHMNBVQWJDYTAKOPR' 'REGKQ' [7,7] -> [0,1] -> [4,3] -> [0,5] -> [1,1]
+# 'QLGCAEKIRLRUGEA' 'REGKQ' Строка должна описывать квадратную матрицу
+# 'QLGNAEK1ILRNGEAE' 'REGKQ' Строка должна содержать только символы алфавита
+# 'eQGNAEKrILRNGEAE' 'REGKQ' [2,2] -> [1,1] -> [0,2] -> [1,2] -> [0,1]
+# 'QLGNAEKI{LRNGEA' 'REGKQ' Строка должна содержать только символы алфавита
+# 'LG.NAEKILLRNGEA' 'REGKQ' Строка должна содержать только символы алфавита
+# 'LGNAEKIL LRNGEA' 'REGKQ' Строка должна содержать только символы алфавита
+# 'WKDODJHFGHGKMDKJXOQTDNTES' '' Вы не ввели слово
+# 'WKDODJHFGHGKMDKJXOQTDNTES' ' ' Слово должно содержать только символы алфавита
+# 'WKDODJHFGHGKMDKJXOQTDNTES' '      ' Слово должно содержать только символы алфавита
+# 'WKDODJHFGHGKMDKJXOQTDNTES' 'QUIC1' Слово должно содержать только символы алфавита
+# 'W' 'W' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# 'W' 'OMNZ' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# 'WE' 'OMNZ' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# 'WEF' 'OMNZ' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# '' 'OMNZ' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# ' ' 'OMNZ' Минимальное количество символов крадратной матрицы должно быть 2 * 2
+# '          ' 'OMNZ' Строка должна содержать только символы алфавита
