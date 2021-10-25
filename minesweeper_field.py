e6344024fe74f5c4041d01fb8f2816ef3d977999
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-10-25 16:44:23 +0300

    ошибка в цикле

--- minesweeper_field.py
+++ minesweeper_field.py
@@ -1,4 +1,5 @@
-net = input().split()
+# net = input().split()
+net = ['4', '4']  # тест
 
 if len(net) != 2:
     print('нужно указать именно два числа')
@@ -15,16 +16,19 @@ n, m = int(net[0]), int(net[1])
 if n < 1 or n > 100 or m < 1 or m > 100:
     print('числа n и m должны быть не меньше 1 и не больше 100')
 
-number_of_lines = ()
+# number_of_lines = ()
+#
+# for _ in range(m):
+#     a = input()
+#     a = list(a)
+#     # print(a)
+#     if len(a) != n:
+#         print(f'кол-во вводимых элементов должно быть равно {n}')
+#         break
+#     number_of_lines += (a,)
 
-for _ in range(m):
-    a = input()
-    a = list(a)
-    print(a)
-    if len(a) != n:
-        print(f'кол-во вводимых элементов должно быть равно {n}')
-        break
-    number_of_lines += (a,)
+number_of_lines = (['.', '.', '*', '.'], ['*', '*', '.', '.'], ['.', '.', '*', '.'], ['.', '.', '.', '.'])  # тест
+
 
 # number_of_cells = n * m
 
@@ -33,46 +37,48 @@ for _ in range(m):
 # print(number_of_lines[-1][-1])
 
 for i in number_of_lines:
-    # ??????????????????????????????
-    print(i)
-    print(f'индекс {number_of_lines.index(i)}')
+    print(f'number_of_lines[i] = {i}')
+    print(f'индекс number_of_lines[i] = {number_of_lines.index(i)}')
     for z in range(len(i)):
-        print(z)
-        print(number_of_lines[number_of_lines.index(i)][z])
+        # print(z)
+        print(f'number_of_lines[{number_of_lines.index(i)}][{z}] = {number_of_lines[number_of_lines.index(i)][z]}')
         if z == 0:
             # print('ноль')
             if i[z] == '.':
                 x = 0
                 # print('.')
-                if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                    x += 1
-                if number_of_lines[number_of_lines.index(i) + 1][z + 1] == '*':
-                    x += 1
-                # print(x)
+                if number_of_lines.index(i) < len(number_of_lines) - 1:
+                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+                        x += 1
+                    if number_of_lines[number_of_lines.index(i) + 1][z + 1] == '*':
+                        x += 1
+                    print(x)
 
         elif z == len(i) - 1:
             # print('минус 1')
             if i[z] == '.':
                 x = 0
                 # print('.')
-                if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                    x += 1
-                if number_of_lines[number_of_lines.index(i) + 1][z - 1] == '*':
-                    x += 1
-                # print(x)
+                if number_of_lines.index(i) < len(number_of_lines) - 1:
+                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+                        x += 1
+                    if number_of_lines[number_of_lines.index(i) + 1][z - 1] == '*':
+                        x += 1
+                    print(x)
 
         else:
             if i[z] == '.':
                 x = 0
                 # print('другое')
-                if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                    x += 1
+                if number_of_lines.index(i) < len(number_of_lines) - 1:
+                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+                        x += 1
                     # print('.')
-                if number_of_lines[number_of_lines.index(i) + 1][z - 1] == 'x':
-                    x += 1
-                if number_of_lines[number_of_lines.index(i) + 1][z + 1] == 'x':
-                    x += 1
-                    # print(x)
+                    if number_of_lines[number_of_lines.index(i) + 1][z - 1] == 'x':
+                        x += 1
+                    if number_of_lines[number_of_lines.index(i) + 1][z + 1] == 'x':
+                        x += 1
+                    print(x)
 
 
 
