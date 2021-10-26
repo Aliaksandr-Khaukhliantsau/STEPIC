6a05978b16340ce7cadfdec89591e80f7f367128
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-10-26 16:24:01 +0300

    ошибка в цикле

--- minesweeper_field.py
+++ minesweeper_field.py
@@ -36,56 +36,119 @@ number_of_lines = (['.', '.', '*', '.'], ['*', '*', '.', '.'], ['.', '.', '*', '
 # print(number_of_lines[-1])
 # print(number_of_lines[-1][-1])
 
-for i in number_of_lines:
-    print(f'number_of_lines[i] = {i}')
-    print(f'индекс number_of_lines[i] = {number_of_lines.index(i)}')
-    for z in range(len(i)):
-        # print(z)
-        print(f'number_of_lines[{number_of_lines.index(i)}][{z}] = {number_of_lines[number_of_lines.index(i)][z]}')
-        if z == 0:
-            # print('ноль')
-            if i[z] == '.':
-                x = 0
-                # print('.')
-                if number_of_lines.index(i) < len(number_of_lines) - 1:
-                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                        x += 1
-                    if number_of_lines[number_of_lines.index(i) + 1][z + 1] == '*':
-                        x += 1
-                    print(x)
+# for i in number_of_lines:
+#     print(f'number_of_lines[i] = {i}')
+#     print(f'индекс number_of_lines[i] = {number_of_lines.index(i)}')
+#     for z in range(len(i)):
+#         print(f'z = {z}')
+#         print(f'number_of_lines[{number_of_lines.index(i)}][{z}] = {number_of_lines[number_of_lines.index(i)][z]}')
+#         if z == 0:
+#
+#             if i[z] == '.':
+#                 x = 0
+#                 if number_of_lines.index(i) < len(number_of_lines):
+#                     if number_of_lines[number_of_lines.index(i)][z + 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z + 1] == '*':
+#                         x += 1
+#                     print(f'x = {x}')
+#
+#         elif z == len(i) - 1:
+#
+#             if i[z] == '.':
+#                 x = 0
+#                 if number_of_lines.index(i) < len(number_of_lines):
+#                     if number_of_lines[number_of_lines.index(i)][z - 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z - 1] == '*':
+#                         x += 1
+#                     print(f'x = {x}')
+#
+#         else:
+#
+#             if i[z] == '.':
+#                 x = 0
+#                 if number_of_lines.index(i) < len(number_of_lines):
+#                     if number_of_lines[number_of_lines.index(i)][z - 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i)][z + 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z - 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) + 1][z + 1] == '*':
+#                         x += 1
+#
+#                 if 0 < number_of_lines.index(i) < len(number_of_lines):
+#                     if number_of_lines[number_of_lines.index(i) - 1][z] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) - 1][z - 1] == '*':
+#                         x += 1
+#                     if number_of_lines[number_of_lines.index(i) - 1][z + 1] == '*':
+#                         x += 1
+#                     print(f'x = {x}')
 
-        elif z == len(i) - 1:
-            # print('минус 1')
-            if i[z] == '.':
-                x = 0
-                # print('.')
-                if number_of_lines.index(i) < len(number_of_lines) - 1:
-                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                        x += 1
-                    if number_of_lines[number_of_lines.index(i) + 1][z - 1] == '*':
-                        x += 1
-                    print(x)
 
-        else:
-            if i[z] == '.':
-                x = 0
-                # print('другое')
-                if number_of_lines.index(i) < len(number_of_lines) - 1:
-                    if number_of_lines[number_of_lines.index(i) + 1][z] == '*':
-                        x += 1
-                    # print('.')
-                    if number_of_lines[number_of_lines.index(i) + 1][z - 1] == 'x':
-                        x += 1
-                    if number_of_lines[number_of_lines.index(i) + 1][z + 1] == 'x':
-                        x += 1
-                    print(x)
+for i in range(len(number_of_lines)):
+    print(f'number_of_lines[{i}] = {number_of_lines[i]}')
+    for z in range(len(number_of_lines[i])):
+        print(f'number_of_lines[{i}][{z}] = {number_of_lines[i][z]}')
+        if i == 0:
+            if z == 0:
+                if number_of_lines[i][z] == '.':
+                    x = 0
+                    if number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i][z + 1] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z + 1] == '*':
+                            x += 1
+                    if 0 < number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i - 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i - 1][z + 1] == '*':
+                            x += 1
+                    print(f'x = {x}')
 
+            elif z == i - 1:
+            # elif z == len(number_of_lines[i]) - 1:
+                if number_of_lines[i][z] == '.':
+                    x = 0
+                    if number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i][z - 1] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z - 1] == '*':
+                            x += 1
+                    if 0 < number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i - 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i - 1][z - 1] == '*':
+                            x += 1
+                    print(f'x = {x}')
 
+            else:
+                if number_of_lines[i][z] == '.':
+                    x = 0
+                    if number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i][z - 1] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i + 1][z - 1] == '*':
+                            x += 1
 
+                    if 0 < number_of_lines.index(number_of_lines[i]) < len(number_of_lines):
+                        if number_of_lines[i - 1][z] == '*':
+                            x += 1
+                        if number_of_lines[i - 1][z - 1] == '*':
+                            x += 1
+                    print(f'x = {x}')
 
-
-
-
-
-if number_of_lines[-1]:
-    pass
