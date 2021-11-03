4c2d9cc72683f5619273292fb7bd4fc6424ee5b6
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-11-03 16:31:10 +0300

    икл while

--- Hanoi_Tower.py
+++ Hanoi_Tower.py
@@ -34,3 +34,56 @@
 # Sample Output 2:
 #
 # 1 - 3
+
+# kernel_1, kernel_2, kernel_3 = [], [], []
+
+# for i in range(int(input())):
+#     kernel_1.append(i + 1)
+
+# print(kernel_1, kernel_2, kernel_3)
+
+
+kernel_1, kernel_2, kernel_3 = [1, 2, 3, 4], [], []
+
+# for x in kernel_1:
+#     if x == 1:
+#         kernel_1.remove(x)
+#         kernel_3.append(x)
+#         continue
+#     kernel_1.remove(x)
+#     kernel_2.append(x)
+
+while True:
+    if kernel_1[0] == 1:
+        kernel_2.append(kernel_1[0])
+        kernel_1.pop(0)
+    else:
+        kernel_3.append(kernel_1[0])
+        kernel_1.pop(0)
+        kernel_3.append(kernel_2[-1])
+        kernel_2.pop(-1)
+        kernel_2.append(kernel_1[0])
+        kernel_1.pop(0)
+        kernel_1.append(kernel_3[-1])
+        kernel_3.pop(-1)
+        kernel_2.append(kernel_3[-1])
+        kernel_3.pop(-1)
+        kernel_2.append(kernel_1[-1])
+        kernel_1.pop(-1)
+        kernel_3.append(kernel_1[-1])
+        kernel_1.pop(-1)
+        kernel_3.append(kernel_2[-1])
+        kernel_2.pop(-1)
+        kernel_1.append(kernel_2[-1])
+        kernel_2.pop(-1)
+        kernel_1.append(kernel_3[-1])
+        kernel_3.pop(-1)
+
+
+
+    # if kernel_1:
+    #     continue
+    # else:
+    #     break
+
+
