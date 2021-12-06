c5b2c79f56850f4b8e79e025e6c1349f6078d159
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-12-06 16:37:37 +0300

    Game_of_life.py 50 %

--- Game_of_life.py
+++ Game_of_life.py
@@ -61,11 +61,48 @@ from pprint import pprint
 
 def func():
     size = tuple(int(i) for i in input().split())
-    field = list(range(size[0]))
+    inp_f = []
+
     for i in range(size[0]):
-        field[i] = input().split()
-        if len(field[i]) != size[1]:
-            return
-    pprint(field)
+        inp_f.append(list(str(i) for i in input()))
+        if len(inp_f[i]) != size[1]:
+            return print(f'величина поля должна быть равна {size[1]}')
+
+
+    out_f = inp_f
+    buff = ''
+
+    for i in range(len(inp_f)):
+        for k in range(len(inp_f[i])):
+            if i == 0 and k == 0:
+                buff = buff + inp_f[i][k] + inp_f[i + 1][k] + inp_f[i + 1][k + 1]  # буфер для проверки
+                if inp_f[i][k] == 'x' and inp_f[i + 1][k] == 'x' and inp_f[i + 1][k + 1] == 'x':
+                    out_f[i][k] = 'x'
+            elif i == 0 and k == len(inp_f[i]) - 1:
+                if inp_f[i][k - 1] == 'x' and inp_f[i + 1][k] == 'x' and inp_f[i + 1][k - 1] == 'x':
+                    out_f[i][k] = 'x'
+            elif i == len(inp_f) - 1 and k == 0:
+                if inp_f[i][k + 1] == 'x' and inp_f[i - 1][k] == 'x' and inp_f[i - 1][k + 1] == 'x':
+                    out_f[i][k] = 'x'
+            elif i == len(inp_f) - 1 and k == len(inp_f[i]) - 1:
+                if inp_f[i][k - 1] == 'x' and inp_f[i - 1][k] == 'x' and inp_f[i - 1][k - 1] == 'x':
+                    out_f[i][k] = 'x'
+            elif i == 0:
+                pass
+            elif k == 0:
+                pass
+            elif i != 0:
+                pass
+            elif k != 0:
+                pass
+            else:
+                pass
+
+    for i in inp_f:
+        print_str = ''
+        for k in i:
+            print_str += k
+        print(print_str)
+
 
 func()
