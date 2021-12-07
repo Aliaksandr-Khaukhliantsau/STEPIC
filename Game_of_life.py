e428e496d391a054db1fd41f16f5eec53acb989c
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-12-07 15:55:47 +0300

    Game_of_life.py 90 %

--- Game_of_life.py
+++ Game_of_life.py
@@ -84,8 +84,8 @@ def func():
     # size = tuple(int(i) for i in input().split())
     # inp_f = []
 
-    # size = [5, 6]
-    size = [5, 5]
+    size = [5, 6]
+    # size = [5, 5]
 
 
     # for i in range(size[0]):
@@ -93,9 +93,9 @@ def func():
     #     if len(inp_f[i]) != size[1]:
     #         return print(f'величина поля должна быть равна {size[1]}')
 
-    # inp_f = [['.', '.', '.', 'X', 'X', '.'], ['.', 'X', 'X', '.', '.', '.'], ['.', '.', 'X', '.', '.', '.'], ['X', 'X', '.', '.', '.', '.'], ['X', '.', '.', 'X', 'X', '.']]
-    inp_f = [['.', '.', '.', '.', '.'], ['.', '.', 'X', '.', '.'], ['.', '.', 'X', '.', '.'], ['.', '.', 'X', '.', '.'],
-             ['.', '.', '.', '.', '.']]
+    inp_f = [['.', '.', '.', 'X', 'X', '.'], ['.', 'X', 'X', '.', '.', '.'], ['.', '.', 'X', '.', '.', '.'], ['X', 'X', '.', '.', '.', '.'], ['X', '.', '.', 'X', 'X', '.']]
+    # inp_f = [['.', '.', '.', '.', '.'], ['.', '.', 'X', '.', '.'], ['.', '.', 'X', '.', '.'], ['.', '.', 'X', '.', '.'],
+    #          ['.', '.', '.', '.', '.']]
 
     for i in inp_f:
         print_str = ''
@@ -110,23 +110,23 @@ def func():
     for i in range(len(inp_f)):
         for k in range(len(inp_f[i])):
             if i == 0 and k == 0:
-                buff += inp_f[i][k + 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1]  # буфер для проверки
+                buff += inp_f[i][len(inp_f[i]) - 1] + inp_f[i + 1][len(inp_f[i]) - 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1] + inp_f[i][k + 1] + inp_f[len(inp_f) - 1][k + 1] + inp_f[len(inp_f) - 1][k] + inp_f[len(inp_f) - 1][len(inp_f[i]) - 1]  # буфер для проверки
             elif i == 0 and k == len(inp_f[i]) - 1:
-                buff += inp_f[i][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][k - 1]
+                buff += inp_f[i][k - 1] + inp_f[i + 1][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][0] + inp_f[i][0] + inp_f[len(inp_f) - 1][0] + inp_f[len(inp_f) - 1][k] + inp_f[len(inp_f) - 1][k - 1]
             elif i == len(inp_f) - 1 and k == 0:
-                buff += inp_f[i][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][k + 1]
+                buff += inp_f[i][len(inp_f[i]) - 1] + inp_f[0][len(inp_f[i]) - 1] + inp_f[0][k] + inp_f[0][k + 1] + inp_f[i][k + 1] + inp_f[i - 1][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][len(inp_f[i]) - 1]
             elif i == len(inp_f) - 1 and k == len(inp_f[i]) - 1:
-                buff += inp_f[i][k - 1] + inp_f[i - 1][k] + inp_f[i - 1][k - 1]
+                buff += inp_f[i][k - 1] + inp_f[0][k - 1] + inp_f[0][k] + inp_f[0][0] + inp_f[i][0] + inp_f[i - 1][0] + inp_f[i - 1][k] + inp_f[i - 1][k - 1]
             elif i == 0:
-                buff += inp_f[i][k - 1] + inp_f[i][k + 1] + inp_f[i + 1][k] + inp_f[i + 1][k - 1] + inp_f[i + 1][k + 1]
+                buff += inp_f[i][k - 1] + inp_f[i + 1][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1] + inp_f[i][k + 1] + inp_f[len(inp_f) - 1][k + 1] + inp_f[len(inp_f) - 1][k] + inp_f[len(inp_f) - 1][k - 1]
             elif i == len(inp_f) - 1:
-                buff += inp_f[i][k - 1] + inp_f[i][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][k - 1] + inp_f[i - 1][k + 1]
+                buff += inp_f[i][k - 1] + inp_f[0][k - 1] + inp_f[0][k] + inp_f[0][k + 1] + inp_f[i][k + 1] + inp_f[i - 1][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][k - 1]
             elif k == 0:
-                buff += inp_f[i - 1][k] + inp_f[i - 1][k + 1] + inp_f[i][k + 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1]
+                buff += inp_f[i][len(inp_f[i]) - 1] + inp_f[i + 1][len(inp_f[i]) - 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1] + inp_f[i][k + 1] + inp_f[i - 1][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][len(inp_f[i]) - 1]
             elif k == len(inp_f[i]) - 1:
-                buff += inp_f[i - 1][k] + inp_f[i - 1][k - 1] + inp_f[i][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][k - 1]
+                buff += inp_f[i][k - 1] + inp_f[i + 1][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][0] + inp_f[i][0] + inp_f[i - 1][0] + inp_f[i - 1][k] + inp_f[i - 1][k - 1]
             else:
-                buff += inp_f[i][k - 1] + inp_f[i + 1][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1] + inp_f[i][k + 1] + inp_f[i - 1][k - 1] + inp_f[i - 1][k] + inp_f[i - 1][k + 1]
+                buff += inp_f[i][k - 1] + inp_f[i + 1][k - 1] + inp_f[i + 1][k] + inp_f[i + 1][k + 1] + inp_f[i][k + 1] + inp_f[i - 1][k + 1] + inp_f[i - 1][k] + inp_f[i - 1][k - 1]
             func_2(inp_f, out_f, buff, i, k)
             buff = ''
 
