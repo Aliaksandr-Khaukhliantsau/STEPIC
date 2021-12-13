835e356e50445933240ed008838d7de05d0e78e6
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-12-13 16:37:14 +0300

    Decimal_number_to_Roman.py 100%

--- adaptive_trainer/Decimal_number_to_Roman.py
+++ adaptive_trainer/Decimal_number_to_Roman.py
@@ -5,7 +5,7 @@
 # 400 и 900 записываются как вычитание из большего числа меньшего: IV, IX, XL, XC, CD и CM, соответственно.
 #
 # Формат ввода:
-# Строка, содержащая натуральное число nn, 0 < n < 40000<n<4000.
+# Строка, содержащая натуральное число nn, 0 < n < 4000
 #
 # Формат вывода:
 # Строка, содержащая число, закодированное в римской системе счисления.
@@ -30,39 +30,35 @@
 # III
 
 def func():
-    # dec_str = input()
-    dec_str = '1984'
-    dec_int = int(dec_str)
-    dec_list = []
-    for i in dec_str:
-        dec_list += i
-
-    dec_str_2 = ''.join(dec_list)
+    dec_str = input()
 
     res = int(dec_str)
     res_list = []
-    i = 1
-    for _ in range(len(dec_str)):
-        buff = str(res)
-        res_list.append(int(buff[len(buff) - i:]))
-        res -= int(buff[len(buff) - i:])
-        print(res)
-        i += 1
+    i = 0
 
+    for _ in dec_str:
+        i += 1
+        res_list.append(str(res)[len(dec_str) - i:])
+        res -= int(str(res)[len(dec_str) - i:])
 
     res_list.reverse()
 
-    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    # rom = {'1': 'I', '5': 'V', '10': 'X', '50': 'L', '100': 'C', '500': 'D', '1000': 'M'}
-
-    print(res_list)
+    rom = {'1': 'I', '2': 'II', '3': 'III', '4': 'IV',
+           '5': 'V', '6': 'VI', '7': 'VII', '8': 'VIII', '9': 'IX',
+           '10': 'X', '20': 'XX', '30': 'XXX', '40': 'XL',
+           '50': 'L', '60': 'LX', '70': 'LXX', '80': 'LXXX', '90': 'XC',
+           '100': 'C', '200': 'CC', '300': 'CCC', '400': 'CD',
+           '500': 'D', '600': 'DC', '700': 'DCC', '800': 'DCCC', '900': 'CM',
+           '1000': 'M', '2000': 'MM', '3000': 'MMM'}
 
     result = ''
     for i in res_list:
-        for k, v in rom:
-            if i == v:
+        for k, v in rom.items():
+            if i == k:
                 result += v
-            elif
+                continue
+
+    print(result)
 
 
 func()
