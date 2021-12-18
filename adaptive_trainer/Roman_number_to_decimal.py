73bbc8b7a784665e532704b87b8912e3a65ba772
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-12-18 20:04:13 +0300

    Roman_number_to_decimal.py 100%

--- adaptive_trainer/Roman_number_to_decimal.py
+++ adaptive_trainer/Roman_number_to_decimal.py
@@ -31,61 +31,53 @@
 #
 # 3
 
-def func():
-    rom = 'III'
-    # rom = input()
-    # rom = ','.join(input())
-    # rom = ','.join(input()).split(',')
-    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    dec_rom = {'1': 'I', '2': 'II', '3': 'III', '4': 'IV',
-           '5': 'V', '6': 'VI', '7': 'VII', '8': 'VIII', '9': 'IX',
-           '10': 'X', '20': 'XX', '30': 'XXX', '40': 'XL',
-           '50': 'L', '60': 'LX', '70': 'LXX', '80': 'LXXX', '90': 'XC',
-           '100': 'C', '200': 'CC', '300': 'CCC', '400': 'CD',
-           '500': 'D', '600': 'DC', '700': 'DCC', '800': 'DCCC', '900': 'CM',
-           '1000': 'M', '2000': 'MM', '3000': 'MMM'}
+
+# первый способ
+
+# def func():
+#     rom = input()
+#     buff = []
+#     result = 0
+#     rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#
+#     for numb in rom:
+#         for k, v in rom_dict.items():
+#             if k == numb:
+#                 buff.append(int(v))
+#
+#     for i in range(len(buff)):
+#         if i == 0:
+#             if len(buff) == 1 or buff[i] >= buff[i + 1]:
+#                 result += buff[i]
+#         elif i < len(buff) - 1:
+#             if buff[i] > buff[i - 1]:
+#                 result += buff[i] - buff[i - 1]
+#             elif buff[i] >= buff[i + 1]:
+#                 result += buff[i]
+#
+#         else:
+#             if buff[i] > buff[i - 1]:
+#                 result += buff[i] - buff[i - 1]
+#             else:
+#                 result += buff[i]
+#
+#     print(result)
+#
+#
+# func()
 
 
-    # rom = reversed(rom)
-    # rom = reversed(rom)
-    # rom.reverse()
+# второй способ
 
+rom = input()
+rom_dec = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+result = 0
 
-    res = []
-    for r in rom:
-        for k, v in rom_dict.items():
-            if k == r:
-                res.append(int(v))
+for i in range(len(rom) - 1):
+    if rom_dec[rom[i]] < rom_dec[rom[i + 1]]:
+        result -= rom_dec[rom[i]]
+    else:
+        result += rom_dec[rom[i]]
 
-    print(res)
-
-
-    result = 0
-    for i in range(len(res)):
-        if i == 0:
-            if res[i] > res[i + 1]:
-                result += res[i]
-            elif res[i] >= res[i + 1]:
-                result += res[i]
-        elif i < len(res) - 1:
-            if res[i] > res[i - 1]:
-                result += (res[i] - res[i - 1])
-            elif res[i] < res[i + 1]:
-                continue
-            elif res[i] >= res[i + 1]:
-                result += res[i]
-            else:
-                pass
-
-        else:
-            if res[i] > res[i - 1]:
-                result += (res[i] - res[i - 1])
-            elif res[i] <= res[i - 1]:
-                result += res[i]
-
-
-
-
-    print(result)
-
-func()
+result += rom_dec[rom[len(rom) - 1]]
+print(result)
