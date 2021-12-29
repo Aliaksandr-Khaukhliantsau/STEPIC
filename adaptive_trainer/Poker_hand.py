0bd83e3bdd6e5b63e03523495f1a6e72b9b999d8
Aliaksandr-Khaukhliantsau <boom646119390@gmail.com>
2021-12-29 16:38:58 +0300

    Poker_hand.py 100%

--- adaptive_trainer/Poker_hand.py
+++ adaptive_trainer/Poker_hand.py
@@ -56,99 +56,60 @@
 #
 # Royal Flush
 
-import collections
+from collections import Counter
+
 
 def func():
-    # my_cards = input().split()
-    # my_cards = ['10C', 'JC', 'QC', 'KC', 'AC']
-    # my_cards = ['4C', '5C', '6C', '7C', '8C']
-    # my_cards = ['10C', '10D', '10H', '10S', 'AC']
-    # my_cards = ['10C', '10H', '10D', 'KC', 'KS']
-    # my_cards = ['2C', 'JC', '8C', '4C', 'AC']
-    # my_cards = ['10C', 'JC', 'QD', 'KH', 'AS']
-    # my_cards = ['10C', '10H', '10D', 'KC', '2C']
-    # my_cards = ['10C', '10H', 'QC', 'QD', 'AC']
-    # my_cards = ['10C', '10H', 'QH', 'KD', 'AS']
-    # my_cards = ['10C', '9H', 'QH', '2D', 'AS']
+    hand = input().split()
+    val_str = '234567891011121314'
+    hand_val = []
+    hand_suit = []
+    hand_val_str = ''
 
+    for card in hand:
+        hand_val.append(card[:-1])
+        hand_suit.append(card[-1])
 
-    my_cards = ['5S', '3H', '6C', '7C', '4S']
+    for i in range(len(hand_val)):
+        if hand_val[i].isdigit():
+            hand_val[i] = int(hand_val[i])
+        elif hand_val[i] == 'J':
+            hand_val[i] = 11
+        elif hand_val[i] == 'Q':
+            hand_val[i] = 12
+        elif hand_val[i] == 'K':
+            hand_val[i] = 13
+        elif hand_val[i] == 'A':
+            hand_val[i] = 14
 
+    hand_val.sort()
 
+    for val in hand_val:
+        hand_val_str += str(val)
 
-    order_value_tuple = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
-    order_value_str = (''.join(order_value_tuple))
-    suit = ('C', 'D', 'H', 'S')
-
-    my_cards_value_list = []
-    my_cards_suit_list = []
-
-
-
-
-    for card in my_cards:
-        my_cards_value_list.append(card[:-1])
-        my_cards_suit_list.append(card[-1])
-
-    my_cards_value_str = (''.join(my_cards_value_list))
-    my_cards_suit_str = (''.join(my_cards_suit_list))
-    my_cards_dict = dict(zip(my_cards_value_list, my_cards_suit_list))
-    my_cards_value_set = set(my_cards_value_list)
-    my_cards_suit_set = set(my_cards_suit_list)
-
-    for i in range(len(my_cards_value_list)):
-        if my_cards_value_list[i].isdigit():
-            my_cards_value_list[i] = int(my_cards_value_list[i])
-        elif my_cards_value_list[i] == 'J':
-            my_cards_value_list[i] = 11
-        elif my_cards_value_list[i] == 'Q':
-            my_cards_value_list[i] = 12
-        elif my_cards_value_list[i] == 'K':
-            my_cards_value_list[i] = 13
-        elif my_cards_value_list[i] == 'A':
-            my_cards_value_list[i] = 14
-
-    my_cards_value_list.sort()
-
-
-
-    print(f'{my_cards_dict}\n{my_cards_value_list}\n{my_cards_suit_list}\n{my_cards_value_set}\n{my_cards_suit_set}')
-
-
-    m_k_v_c = collections.Counter(my_cards_value_list)
-    m_k_s_c = collections.Counter(my_cards_suit_list)
-
-
-    print(m_k_v_c)
-    print(m_k_s_c)
-    #
-    print(m_k_v_c.values())
-    print(m_k_s_c.values())
-
-    if len(m_k_s_c) == 1:
-        if my_cards_value_list == ['10', 'J', 'Q', 'K', 'A']:
+    if len(set(hand_suit)) == 1:
+        if hand_val == [10, 11, 12, 13, 14]:
             print('Royal Flush')
-        elif my_cards_value_str in order_value_str:
+        elif hand_val_str in val_str:
             print('Straight Flush')
-        elif len(my_cards_value_set) == 5:
+        elif len(set(hand_val)) == 5:
             print('Flush')
 
     else:
-        if my_cards_value_str in order_value_str:
+        if hand_val_str in val_str:
             print('Straight')
-        elif set(m_k_v_c.values()) == {4, 1}:
+        elif set(Counter(hand_val).values()) == {4, 1}:
             print('Four of a Kind')
-        elif set(m_k_v_c.values()) == {3, 2}:
+        elif set(Counter(hand_val).values()) == {3, 2}:
             print('Full House')
-        elif set(m_k_v_c.values()) == {3, 1}:
+        elif set(Counter(hand_val).values()) == {3, 1}:
             print('Three of a Kind')
-        elif list(m_k_v_c.values()).count(2) == 2:
+        elif list(Counter(hand_val).values()).count(2) == 2:
             print('Two Pairs')
-        elif list(m_k_v_c.values()).count(1) == 3:
+        elif list(Counter(hand_val).values()).count(1) == 3:
             print('Pair')
         else:
             print('High Card')
-
 
 
 func()
